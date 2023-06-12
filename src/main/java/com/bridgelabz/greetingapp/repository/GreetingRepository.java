2736dde4430f93b09749b54da45f63428f9df3f6
Sourav2707 <smartsourav33@gmail.com>
2023-06-12 13:53:51 +0530

    UC1 Create Spring App for Hello Messages using different HTTP Methods. Test using curl

---
+++ src/main/java/com/bridgelabz/greetingapp/repository/GreetingRepository.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.greetingapp.repository;
+
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public class GreetingRepository extends JpaRepositoriesAutoConfiguration {
+}
